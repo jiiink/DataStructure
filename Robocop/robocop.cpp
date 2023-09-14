99722b799dc778be63ab866f15f4fca55db46a2e
김병현 <kimbyeonghyeon@myBook.local>
2023-09-14 14:22:46 +0900

    .

--- Robocop/robocop.cpp
+++ Robocop/robocop.cpp
@@ -6,8 +6,8 @@ using namespace std;
 int numOfPoints;
 vector< pair<int, int> > points;
 int times[5];
-vector<int> distances;
-vector<float> each_distances;
+vector<int> distances_from_start;
+vector<int> each_distances;
 
 
 void output(int x, int y) {
@@ -16,16 +16,16 @@ void output(int x, int y) {
 
 void get_distances() {
     points.push_back(points[0]);
-    distances.push_back(0);
+    distances_from_start.push_back(0);
     int distance_from_start = 0;
     int each_distance = 0;
 
 
     for (int i=0; i<points.size()-1; i++) {
-        float each = (abs(points[i].first - points[i+1].first) + abs(points[i].second - points[i+1].second));
-        each_distances.push_back(each);
-        each_distance += each;
-        distances.push_back(each_distance);
+        each_distance = (abs(points[i].first - points[i+1].first) + abs(points[i].second - points[i+1].second));
+        each_distances.push_back(each_distance);
+        distance_from_start += each_distance;
+        distances_from_start.push_back(distance_from_start);
     }
 }
 
@@ -34,20 +34,20 @@ void run_robo(int time) {
     vector<pair<int, int> > directions;
     int time_minus_distance = 0;
     int i;
-    for (i=0; i<distances.size(); i++) {
-        if (time <= distances[i]) {
-            time_minus_distance = time - distances[i-1];
+    for (i=0; i<distances_from_start.size(); i++) {
+        if (time <= distances_from_start[i]) {
+            time_minus_distance = time - distances_from_start[i-1];
             break;
         }
     }
-    directions.emplace_back(points[i-1].first + time_minus_distance, points[i-1].second); // + x
-    directions.emplace_back(points[i-1].first - time_minus_distance, points[i-1].second); // - x
-    directions.emplace_back(points[i-1].first, points[i-1].second + time_minus_distance); // + y
-    directions.emplace_back(points[i-1].first, points[i-1].second - time_minus_distance); // - y
+    directions.emplace_back(points[i-1].first + time_minus_distance, points[i-1].second); // + x direction
+    directions.emplace_back(points[i-1].first - time_minus_distance, points[i-1].second); // - x direction
+    directions.emplace_back(points[i-1].first, points[i-1].second + time_minus_distance); // + y direction
+    directions.emplace_back(points[i-1].first, points[i-1].second - time_minus_distance); // - y direction
 
-    for (int j=0; j<4; j++) {
-        float dist = hypot(directions[j].first - points[i].first, directions[j].second - points[i].second);
-        if (dist <= each_distances[i-1]) {
+    for (int j=0; j<directions.size(); j++) {
+        float distance = hypot(directions[j].first - points[i].first, directions[j].second - points[i].second);
+        if (distance <= each_distances[i-1]) { // the correct direction to the next point
             output(directions[j].first, directions[j].second);
         }
     }
@@ -75,7 +75,7 @@ int main() {
     input();
     get_distances();
     for (auto& time : times) {
-        time %= distances[numOfPoints];
+        time %= distances_from_start[numOfPoints];
         run_robo(time);
     }
 
