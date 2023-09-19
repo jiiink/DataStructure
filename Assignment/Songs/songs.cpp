cc8922de714d42a3aacc053a9985fa42e282c5d9
김병현 <kimbyeonghyeon@myBook.local>
2023-09-20 00:55:13 +0900

    .

--- Assignment/Songs/songs.cpp
+++ Assignment/Songs/songs.cpp
@@ -15,7 +15,25 @@ class Song {
         void print() {
             cout << this->title << " " << this->genre << " " << this->broadcasting << " " << this->size << " " << this->download << endl;
         }
+        ~Song() {}
 };
+
+int n = 0;
+int ranking = 0;
+
+
+
+bool mysort(Song i, Song j) {
+    if (i.broadcasting > j.broadcasting) {
+        return i.broadcasting > j.broadcasting;
+    }
+    if (i.broadcasting == j.broadcasting && i.download > j.download) {
+        return i.download > j.download;
+    }
+    if (i.broadcasting == j.broadcasting && i.download == j.download && i.size < j.size) {
+        return i.size < j.size;
+    }
+}
 
 vector<Song> songs;
 
@@ -24,11 +42,11 @@ void input() {
     string first_line;
     getline(cin, first_line);
     istringstream iss(first_line);
-    int n = 0;
-    int ranking = 0;
+    
+    
     iss >> n >> ranking;
 
-    // cout << n << " " << ranking << endl;
+    
     
     while (!cin.eof()) {
         string line;
@@ -49,7 +67,7 @@ void input() {
 
         songs.emplace_back(title, genre, broadcasting, size, download);
     }
-
+    cout << n << " " << ranking << endl;
 
     // cout << "\n--------------\n"; 
     // for (auto& song : songs) {
@@ -58,8 +76,31 @@ void input() {
 
 }
 
+void output() {
+    int ranking_index = ranking - 1;
+    if (ranking == 1) {
+        cout << songs[ranking_index].title << endl;
+        return;
+    }
+    // if (songs[ranking_index].genre == songs[ranking_index-1].genre) {
+
+    // }
+    while (songs[ranking_index].genre == songs[ranking_index-1].title) {
+        ranking_index++;
+    }
+    cout << songs[ranking_index].title << endl;
+}
+
 int main() {
     input();
+    sort(songs.begin(), songs.end(), mysort);
+
+    cout << "\n--------------\n"; 
+    for (auto& song : songs) {
+        song.print();
+    }
+
+    output();
 
     return 0;
 }
