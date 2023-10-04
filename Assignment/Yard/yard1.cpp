fa19918f61346c6615d8b2e19c7fb5ce0a0d0a14
jiiink <bhkim0404@gmail.com>
2023-10-04 10:13:27 +0900

    .

---
+++ Assignment/Yard/yard1.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void output() {
+
+}
+
+
+void process() {
+
+}
+
+
+void input() {
+
+}
+
+int main() {
+	input();
+	process();
+	output();
+	return 0;
+}
