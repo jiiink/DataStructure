58890e994ec519fd9a44057ecdffa38254496239
jiiink <bhkim0404@gmail.com>
2023-10-17 18:25:03 +0900

    .

--- Assignment/Taxfree/taxfree.cpp
+++ Assignment/Taxfree/taxfree.cpp
@@ -2,16 +2,14 @@
 #include <stack>
 #include <set>
 #include <vector>
-// #include <algorithm>
 using namespace std;
-// #define allout(m) for(auto& i : m) cout << i << " "; cout << endl;
-// score 83
 
-multiset<int> items;
+multiset<int> items; // store the sorted values
 int K;
 int total = 0;
 stack<int> shoppingList;
 set< vector<int> > completeList;
+set<vector<int>>::iterator maxIT;
 
 void input() {
     int N;
@@ -23,13 +21,9 @@ void input() {
     }
 }
 
-
-
 void totalSum(vector<int> items) {
     total = 0;
-    for (auto& i : items) {
-        total += i;
-    }
+    for (auto& i : items) total += i;
 }
 
 vector<int> stackToVector(stack<int> s) {
@@ -42,77 +36,62 @@ vector<int> stackToVector(stack<int> s) {
 }
 
 void process(multiset<int> items) {
-    
     for (auto it=items.begin(); it!=items.end(); ++it) {
-
         shoppingList.push(*it);
         vector<int> v = stackToVector(shoppingList);
         totalSum(v);
 
         if (total == K) {
-            
             completeList.insert(v);
             shoppingList.pop();
         } else if (total < K) {
             multiset<int> copy = items;
             multiset<int>::iterator copy_it = copy.begin();
             
-            while (*copy_it != *it) {
-                copy_it++;
-            }
-            copy.erase(copy.begin(), ++copy_it);
+            while (*copy_it != *it) copy_it++;
 
+            copy.erase(copy.begin(), ++copy_it);
             
             process(copy);
-
         } else if (total > K) shoppingList.pop();
-        
     }
     if (shoppingList.empty()) return;
     else shoppingList.pop();
-    
 }
 
 
 
-void output(set<vector<int>>::iterator maxIT) {
-   
+void output() {
     if (completeList.empty()) {
         cout << "0\n";
         return;
     } 
-  
     for (auto it = (*maxIT).rbegin(); it != (*maxIT).rend(); it++) {
         cout << *it << endl;
     }
 }
 
 void findMaxStack() {
-    int maxV = 0;
-    // auto copyCL = completeList;
-   
-    auto maxIT = completeList.begin();
+    int maxPrice = 0;
+    maxIT = completeList.begin();
+
     for (auto it=completeList.begin(); it!=completeList.end(); ++it) {
-        if ((*it).front() > maxV) {
-            maxV = (*it).front();
-            
+        if ((*it).front() > maxPrice) {
+            maxPrice = (*it).front();
             maxIT = it;
-        } else if ((*it).front() == maxV) {
+        } else if ((*it).front() == maxPrice) {
             int i = 1;
             while (i++) {
                 if ((*maxIT)[i] != (*it)[i]) break;
-                // i++;
             }
             if ((*maxIT)[i] < (*it)[i]) maxIT = it;
         }
     }
-    output(maxIT);
 }
 
 int main() {
     input();
-
     process(items);
     findMaxStack();
-
+    output();
 }
