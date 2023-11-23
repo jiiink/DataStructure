31e14c6368dbcc129ac740cd1735a38db999562f
jiiink <bhkim0404@gmail.com>
2023-11-23 21:07:11 +0900

    .

--- Assignment/Support/support1.cpp
+++ Assignment/Support/support1.cpp
@@ -11,11 +11,17 @@ class mycomparison {
     public:
         mycomparison() {}
         bool operator() (const Guest& lhs, const Guest& rhs) {
-            return lhs.cT < rhs.cT;
+            if (lhs.cT == rhs.cT) {
+                return lhs.aT > rhs.aT;
+            } else {
+                return lhs.cT < rhs.cT;
+            }
         }
 };
 priority_queue<Guest, vector<Guest>, mycomparison> Room;
+queue<Guest> fromInput;
 vector<Guest> outRoom;
+int t = 30;
 
 
 void input() {
@@ -25,7 +31,11 @@ void input() {
     while (N--) {
         cin >> aT >> id >> cT;
         Guest guest(aT, id, cT);
-        Room.push(guest);
+        if (guest.aT <= t) { // 
+            Room.push(guest);
+        } else {
+            fromInput.push(guest);
+        }
     }
 }
 
@@ -40,22 +50,56 @@ void printPQ() {
 
 
 void council(Guest& g) {
-    if (g.cT > 10) {
-        g.cT -= g.cT/2;
-        g.aT += g.cT;
+    int councilTime = 0;
+    int minCouncilTime = 10;
+    if (g.cT > minCouncilTime) {
+        councilTime = g.cT/2;
+        g.cT -= councilTime;
+        t += councilTime;
+        g.aT = t;
         Room.push(g);
     } else {
+        // councilTime = 10;
+        t += g.cT;
         outRoom.push_back(g);
     }
+    // t += councilTime;
+    // g.aT = t;
 }
 
 
 void process() {
     // auto room_cp = Room;
-    while (!Room.empty()) {
-        Guest currentGuest = Room.top();
-        Room.pop();
-        council(currentGuest);
+    // while (!Room.empty()) {
+    //     Guest currentGuest = Room.top();
+    //     Room.pop();
+    //     council(currentGuest);
+    // }
+    while (true) {
+        while (!Room.empty()) {
+            Guest currentGuest = Room.top();
+            Room.pop();
+            council(currentGuest);
+            // if (fromInput.front().aT <= t) {
+            //     Room.push(fromInput.front());
+            //     fromInput.pop();
+            // }
+            while (!fromInput.empty()) {
+                if (fromInput.front().aT <= t) {
+                    Room.push(fromInput.front());
+                    fromInput.pop();
+                } else {
+                    break;
+                }
+            }
+        }
+        if (fromInput.empty()) {
+            break;
+        } else {
+            t = fromInput.front().aT;
+            Room.push(fromInput.front());
+            fromInput.pop();
+        }
     }
 }
 
@@ -66,8 +110,9 @@ int main() {
     process();
 
     for (auto g : outRoom) {
-        cout << g.id << " ";
+        cout << g.id << endl;
     }
+    // cout << 27 / 2;
 
     return 0;
 }
