439c367aba4e6e76921a55aaba14807f5d777cea
jiiink <bhkim0404@gmail.com>
2023-12-12 16:51:29 +0900

    .

--- Final/prob1.cpp
+++ Final/prob1.cpp
@@ -2,7 +2,7 @@
 #define myout(msg, x) cout << "\n"<<msg<<":";for (auto s : x) cout<<" "<<s;
 using namespace std;
 
-void a() {
+int main() {
     string sa = "Pizza", sb = "Cola", sc = "World-Cup";
     list<char> la (sa.begin(), sa.end());
     list<char> lb (sb.begin(), sb.end());
@@ -10,13 +10,36 @@ void a() {
 
     la.splice(la.end(), lb);
 
+    // a
     myout("la[]=", la);
     myout("lb[]=", lb);
-}
 
-int main() {
 
-    a();
+    list<char>::iterator ita, it1, it2;
+    it1 = lc.end(); it1--; it1--; it1--; it1--;
+    la.splice(lc.begin(), lc, it1, lc.end());
+    // b
+    myout("lc[]= ", lc);
+
+
+
+    string mystr = "PUSANPOP_SUMMER";
+    list<char> mylist (mystr.begin(), mystr.end());
+    it1 = find(mylist.begin(), mylist.end(), 'S');
+    it2 = find(mylist.begin(), mylist.end(), 'M');
+    // c
+    cout << "\n" << "dist= " << distance(it1, it2);
+
+
+    string sd = "abcdefg12345";
+    list<char> tlist(sa.begin(), sa.end());
+    list<char> ld(sd.begin(), sd.end());
+    it1 = ld.begin();
+    it2 = tlist.begin();
+    advance(it1, 3);
+    tlist.splice(++it2, ld, ld.begin(), it1);
+    // d
+    myout("\n tlist= ", tlist);
 
     return 1;
 }
